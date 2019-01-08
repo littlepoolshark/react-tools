3ec84cf7a935d9a1c2f1f86c5c390dfe0e330389
刘其深 <qishen_liu@sui.com>
2019-01-08 19:00:36 +0800

    initialization

---
+++ stories/index.stories.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+// import { Button } from '@storybook/react/demo';
+
+storiesOf('Button', module)
+  .add('with text', () => (
+    <div>1</div>
+  ))
+  .add('with some emoji', () => (
+    <div>2</div>
+  ));
